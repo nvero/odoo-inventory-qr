3201de974947d044493eb8870717fa9a6cee8f57
Omar Mahmoud <omr.sayed@gmail.com>
2021-04-18 14:01:39 +0400

    qr per product

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -6,10 +6,12 @@ class OsmInventoryQr(http.Controller):
 
     @http.route('/inventory/qr/', auth='public', website=True)
     def index2(self, **kw):
+        picking_id = kw.get('picking_id')
         id = kw.get('id')
-        data = http.request.env['stock.picking'].search([('id','=', id)])
+        data = http.request.env['stock.picking'].search([('id','=', picking_id)])
         return http.request.render('osm__inventory_qr.qr_page', {
             'root': '/osm__inventory_qr/osm__inventory_qr',
             'docs': data,
+            'move_id': id
         })
 
--- models/qr_code_invoice.py
+++ models/qr_code_invoice.py
@@ -6,14 +6,14 @@ from odoo.addons.osm__inventory_qr.models.qr_code_base import generate_qr_code
  
 
 class QRCodeInvoice(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = 'stock.move'
 
     qr_image = fields.Binary("QR Code", compute='_generate_qr_code')
     qr_in_report = fields.Boolean('Show QR in Report')
 
     def _generate_qr_code(self):
         base_url = request.env['ir.config_parameter'].get_param('web.base.url')
-        base_url += '/inventory/qr/?id=%s' % (self.id,)
+        base_url += '/inventory/qr/?picking_id=%s&id=%s' % (self.picking_id.id,self.id)
         self.qr_image = generate_qr_code(base_url)
 
 
